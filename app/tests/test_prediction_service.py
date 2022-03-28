2afb729caeef6819e36a5bc05372a63778ddcb03
Linda <lindavik@users.noreply.github.com>
2022-03-29 00:08:01 +0200

    006 create endpoint (#20)

--- app/tests/test_prediction_service.py
+++ app/tests/test_prediction_service.py
@@ -1,12 +1,8 @@
 import pytest
 
+from app.converters import MissionDetails
 from app.prediction_service import (
-    PlanetGraph,
-    _convert_capture_probability_to_success_rate,
-    _get_capture_attempt_count,
-    _get_probability_of_capture,
-    get_probability_of_success,
-    _get_shortest_path_to_destination,
+    PlanetGraph, PredictionService,
 )
 from tests.shared_test_utils import HOTH, TATOOINE, ENDOR, planet_graph
 
@@ -16,19 +12,19 @@ def hunter_schedule():
     return {HOTH: {6, 7, 8}}
 
 
-def test_get_shortest_path_to_destination_extended(planet_graph):
-    autonomy: int = 6
-    departure = TATOOINE
-    destination = ENDOR
+@pytest.fixture
+def prediction_service(planet_graph):
+    mission_details: MissionDetails = MissionDetails(autonomy=6,
+                                                     arrival=ENDOR,
+                                                     departure=TATOOINE,
+                                                     routes=planet_graph)
+    return PredictionService(mission_details=mission_details)
 
+
+def test_get_shortest_path_to_destination_extended(prediction_service):
     expected_route = {TATOOINE: 0, HOTH: 6, ENDOR: 8}
 
-    route = _get_shortest_path_to_destination(
-        planet_graph=planet_graph,
-        departure=departure,
-        destination=destination,
-        autonomy=autonomy,
-    )
+    route = prediction_service._get_shortest_path_to_destination()
 
     assert route == expected_route
 
@@ -60,18 +56,15 @@ def planet_graph_minimal():
 
 
 def test_get_shortest_path_to_destination_minimal(planet_graph_minimal):
-    autonomy: int = 6
-    departure = TATOOINE
-    destination = ENDOR
+    mission_details: MissionDetails = MissionDetails(autonomy=6,
+                                                     arrival=ENDOR,
+                                                     departure=TATOOINE,
+                                                     routes=planet_graph_minimal)
+    prediction_service = PredictionService(mission_details=mission_details)
 
     expected_route = {TATOOINE: 0, ENDOR: 6}
 
-    route = _get_shortest_path_to_destination(
-        planet_graph=planet_graph_minimal,
-        departure=departure,
-        destination=destination,
-        autonomy=autonomy,
-    )
+    route = prediction_service._get_shortest_path_to_destination()
 
     assert route == expected_route
 
@@ -80,19 +73,10 @@ def test_get_shortest_path_to_destination_minimal(planet_graph_minimal):
     "input_countdown, expected",
     [(10, 90), (9, 90), (7, 0), (6, 0)],
 )
-def test_get_probability_of_success(
-    planet_graph, input_countdown, expected, hunter_schedule
+def test_get_probability_of_success(prediction_service, input_countdown, expected, hunter_schedule
 ):
-    autonomy: int = 6
-    departure = TATOOINE
-    destination = ENDOR
-
-    actual = get_probability_of_success(
+    actual = prediction_service.get_probability_of_success(
         countdown=input_countdown,
-        graph=planet_graph,
-        departure=departure,
-        destination=destination,
-        autonomy=autonomy,
         hunter_schedule=hunter_schedule,
     )
 
@@ -104,7 +88,7 @@ def test_get_probability_of_success(
     [(1, 0.1), (2, 0.19), (3, 0.271), (0, 0)],
 )
 def test_get_probability_of_capture(input, expected):
-    actual = _get_probability_of_capture(input)
+    actual = PredictionService._get_probability_of_capture(input)
 
     assert actual == expected
 
@@ -114,7 +98,7 @@ def test_get_probability_of_capture(input, expected):
     [(0.231, 77), (0, 100), (1.0, 0), (0.19, 81), (0.11789, 88)],
 )
 def test_convert_capture_probability_to_success_rate(input, expected):
-    actual = _convert_capture_probability_to_success_rate(input)
+    actual = PredictionService._convert_capture_probability_to_success_rate(input)
 
     assert actual == expected
 
@@ -123,7 +107,7 @@ def test__get_capture_attempt_count_with_capture(hunter_schedule):
     shortest_path = {TATOOINE: 0, HOTH: 7, ENDOR: 9}
     expected: int = 1
 
-    actual = _get_capture_attempt_count(
+    actual = PredictionService._get_capture_attempt_count(
         shortest_path=shortest_path, hunter_schedule=hunter_schedule
     )
 
@@ -135,12 +119,11 @@ def test__get_capture_attempt_count_without_capture():
     shortest_path = {TATOOINE: 0, HOTH: 7, ENDOR: 9}
     expected: int = 0
 
-    actual = _get_capture_attempt_count(
+    actual = PredictionService._get_capture_attempt_count(
         shortest_path=shortest_path, hunter_schedule=hunter_schedule
     )
 
     assert actual == expected
-
 
 # @pytest.fixture
 # def planet_graph_extended_v2():
@@ -168,4 +151,3 @@ def test__get_capture_attempt_count_without_capture():
 #     )
 #
 #     assert route == expected_route
-
