8ff17f3bd3a3393b96598a60206e6877ab445918
lindavik <linda.viksne@gmail.com>
2022-03-28 20:43:24 +0200

    004 - refactoring

--- app/app/file_reader.py
+++ app/app/file_reader.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import pathlib
+from typing import Dict
 
 logging.getLogger().addHandler(logging.StreamHandler())
 
@@ -8,7 +9,12 @@ logging.getLogger().addHandler(logging.StreamHandler())
 class FileReader:
 
     @staticmethod
-    def read_json(path_to_file: str):
+    def read_json(path_to_file: str) -> Dict:
+        """
+        Reads in a json file and returns a dictionary representation of the file content.
+        :param path_to_file: path to the json file
+        :return: the file content
+        """
         file_extension = pathlib.Path(path_to_file).suffix
         if file_extension == ".json":
             try:
--- app/app/main.py
+++ app/app/main.py
@@ -7,10 +7,10 @@ logging.getLogger().addHandler(logging.StreamHandler())
 
 
 def main():
-    mission_details_file: str = ""
+    mission_details_file: str = "" #todo populate with args
     mission_details = ContextLoader.load_mission_details(file_path=mission_details_file)
-    intercepted_data_file: str = ""
-    intercepted_data = ContextLoader.load_intercepted_data(file=intercepted_data_file)
+    intercepted_data_file: str = "" #todo populate with args
+    intercepted_data = ContextLoader.load_intercepted_data(file_path=intercepted_data_file)
     mission = Mission(mission_details=mission_details, intercepted_data=intercepted_data)
     outcome = mission.get_mission_outcome()
 
