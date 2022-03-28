9413eaf721b05a825dc2c95610842eac5b93e4b7
lindavik <linda.viksne@gmail.com>
2022-03-28 20:32:18 +0200

    004 - add tests

--- app/app/context.py
+++ app/app/context.py
@@ -1,48 +1,28 @@
 import os
 
 from app.file_reader import FileReader
-from app.converters import MissionConverter, MissionDetails
-
-
-# {
-#   "countdown": 6,
-#   "bounty_hunters": [
-#     {"planet": "Tatooine", "day": 4 },
-#     {"planet": "Dagobah", "day": 5 }
-#   ]
-# }
-#
-#
-# class BountyHunterDTO:
-#     def __init__(self, planet: str, day: int):
-#         self.planet = countdown
-#         self.bounty_hunters = bounty_hunters
-
-
-# class InterceptedDataDTO:
-#     def __init__(self, countdown: int, bounty_hunters: List):
-#         self.countdown = countdown
-#         self.bounty_hunters = bounty_hunters
-#
-#     def __eq__(self, other):
-#         """Overrides the default implementation"""
-#         if isinstance(other, InterceptedDataDTO):
-#             return (self.countdown == other.countdown and
-#                     self.bounty_hunters == other.bounty_hunters)
-#
-#         return False
+from app.converters import MissionConverter, MissionDetails, InterceptedData, InterceptedDataConverter
 
 
 class ContextLoader:
 
     @staticmethod
-    def load_mission_details(file_path: str):
+    def load_mission_details(file_path: str) -> MissionDetails:
+        """
+        Loads processed and validated mission details from an input file.
+        :param file_path: path to the input file containing the mission details
+        :return: mission details
+        """
         mission_details_raw = FileReader.read_json(file_path)
         directory: str = os.path.dirname(file_path)
-        mission_details: MissionDetails = MissionConverter.map_to_mission_details(mission_details_raw, directory)
-        return mission_details
+        return MissionConverter.map_to_mission_details(mission_details_raw, directory)
 
     @staticmethod
-    def load_intercepted_data(file):
-        raw_intercepted_data = FileReader.read_json(file)
-        pass
+    def load_intercepted_data(file_path: str) -> InterceptedData:
+        """
+        Loads processed and validated intercepted data from an input file.
+        :param file_path: path to the input file containing the intercepted data
+        :return: intercepted data
+        """
+        raw_intercepted_data = FileReader.read_json(file_path)
+        return InterceptedDataConverter.map_to_intercepted_data(raw_data=raw_intercepted_data)
