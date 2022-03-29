ceac592c8bdfc79404b25693f87b54132dceeadb
Linda <lindavik@users.noreply.github.com>
2022-03-29 14:05:07 +0200

    Revert "100 setup frontend (#21)" (#22)
    This reverts commit 1c6d448efd99a9b2864d706f6ad9217bbeb1439c.

--- app/scripts.py
+++ app/scripts.py
@@ -2,12 +2,4 @@ import os
 
 
 def test():
-    os.system("python -m pytest")
-
-
-def reformat():
-    os.system("shed --refactor")
-
-
-def coverage():
-    os.system("pytest --cov=app --cov-fail-under=95 tests")
+    os.system('pytest')
