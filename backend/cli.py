0d9947a4ec99e7e5461794765a1ad29bfb8b78d0
lindavik <linda.viksne@gmail.com>
2022-03-30 09:32:14 +0200

    200 - add documentation

--- backend/cli.py
+++ backend/cli.py
@@ -3,9 +3,17 @@ from pathlib import Path
 import typer
 
 
-def main(millennium_falcon_file_path: Path, empire_file_path: Path):
+def get_odds(
+        millennium_falcon_file_path: Path = typer.Argument(...,
+                                                           help="The path to the file containing the Millennium Falcon mission details"),
+        empire_file_path: Path = typer.Argument(...,
+                                                help="The path to the file containing the rebel intercepted data")):
+    """
+    Gives the odds of the Millennium Falcon reaching the destination planet in time without getting captured by bounty hunters.
+    Takes two input files: a file containing the Millennium Falcon mission details and a file containing the rebel intercepted data.
+    """
     typer.echo(f"Hello {millennium_falcon_file_path}, {empire_file_path}")
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    typer.run(get_odds)
