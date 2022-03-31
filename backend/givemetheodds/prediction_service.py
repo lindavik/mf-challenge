2cb72f4e8865fa43fce8a6fefbae9d184a2fbeba
lindavik <linda.viksne@gmail.com>
2022-03-31 20:22:32 +0200

    008 - update algorithm

--- backend/givemetheodds/prediction_service.py
+++ backend/givemetheodds/prediction_service.py
@@ -32,9 +32,16 @@ class PredictionService(object):
             return 0
         else:
             delay_budget: int = countdown - earliest_arrival_day
-            capture_attempt_count = PredictionService._get_lowest_capture_attempt_count(
-                shortest_path=adjusted_path, hunter_schedule=hunter_schedule, delay_budget=delay_budget
+
+            shortest_path = self._get_shortest_path_to_destination_avoiding_bounty_hunters(hunter_schedule)
+
+            capture_attempt_count = PredictionService._get_capture_attempt_count(
+                shortest_path=adjusted_path, hunter_schedule=hunter_schedule
             )
+
+            # capture_attempt_count = PredictionService._get_lowest_capture_attempt_count(
+            #     shortest_path=adjusted_path, hunter_schedule=hunter_schedule, delay_budget=delay_budget
+            # )
 
             probability_of_capture: float = PredictionService._get_probability_of_capture(
                 capture_attempt_count=capture_attempt_count
@@ -42,6 +49,57 @@ class PredictionService(object):
             return PredictionService._convert_capture_probability_to_success_rate(
                 probability_of_capture=probability_of_capture
             )
+
+    def _get_shortest_path_to_destination_avoiding_bounty_hunters(self, hunter_schedule: Dict):
+        """
+        Determines the shortest path from the departure planet to the destination planet.
+        Disclaimer: provides with only one route, if there are two equally long shortest routes, only one will be returned.
+        :return:
+        """
+        visited = {self.departure: 0}
+        path = defaultdict(list)
+        nodes = set(self.planet_graph.planets)
+
+        while nodes:
+            closest_node = None
+            for node in nodes:
+                if node in visited:
+                    if closest_node is None:
+                        closest_node = node
+                    elif visited[node] < visited[closest_node]:
+                        closest_node = node
+            if closest_node is None:
+                break
+
+            nodes.remove(closest_node)
+            current_distance_travelled = visited[closest_node]
+
+            for edge in self.planet_graph.routes[closest_node]:
+
+                new_distance = (
+                        current_distance_travelled + self.planet_graph.distances[(closest_node, edge)]
+                )
+
+                if edge not in visited or new_distance < visited[edge]:
+                    visited[edge] = new_distance
+                    path[edge] = []
+                    path[edge].append(closest_node)
+
+        while self.departure not in path[self.destination]:
+            for item in path[self.destination]:
+                item_source = path[item]
+                for subitem in item_source:
+                    path[self.destination].append(subitem)
+
+        shortest_path = path[self.destination]
+
+        shortest_route: List = []
+        for item in shortest_path:
+            shortest_route.append((item, visited[item]))
+        shortest_route.append((self.destination, visited[self.destination]))
+
+        # sort shortest route by day in ascending order
+        return sorted(shortest_route, key=lambda item: item[1])
 
     def _get_shortest_path_to_destination(self):
         """
